76f70424ac00ad56c599462deb5a5f3ca2ad42f8
Gois <matheusgoislimasilva@gmail.com>
2019-11-06 18:36:58 -0300

    fix

--- Sources/SwiftFirmata/SwiftFirmata.swift
+++ Sources/SwiftFirmata/SwiftFirmata.swift
@@ -2,16 +2,20 @@ import Firmata
 import Foundation
 
 public enum DigitalLevel: Int {
-  case Low, High
+    case Low, High
 }
 
 public enum PinMode: Int {
-  case Input, Output
+    case Input, Output
 }
 
 public struct Pin {
-  var number: Int
-  var mode: PinMode
+    var number: Int
+    var mode: PinMode
+    public init(number: Int,mode: PinMode){
+        self.number = number
+        self.mode = mode
+    }
 }
 
 public enum SwiftFirmataError: Error {
@@ -19,33 +23,33 @@ public enum SwiftFirmataError: Error {
 }
 
 public class SwiftFirmata {
-  private var firmataC: UnsafeMutablePointer<t_firmata>?
-
-  public init(connect: String, baud: Int) throws {
-    self.firmataC = firmata_new(strdup(connect))
-
-    if self.firmataC == nil {
-      throw SwiftFirmataError.couldntConnect
+    private var firmataC: UnsafeMutablePointer<t_firmata>?
+    
+    public init(connect: String, baud: Int) throws {
+        self.firmataC = firmata_new(strdup(connect))
+        
+        if self.firmataC == nil {
+            throw SwiftFirmataError.couldntConnect
+        }
     }
-  }
-
-  deinit {
-    if self.firmataC != nil {
-      free(self.firmataC)
+    
+    deinit {
+        if self.firmataC != nil {
+            free(self.firmataC)
+        }
     }
-  }
-
-  public func configure(pin: Pin) {
-    firmata_pinMode(firmataC, Int32(pin.number), Int32(pin.mode.rawValue))
-  }
-
-  public func digitalWrite(pin: Pin, level: DigitalLevel) {
-    firmata_digitalWrite(firmataC, Int32(pin.number), Int32(level.rawValue))
-  }
-
-  public func analogWrite(pin: Pin, width: Int) {
-    firmata_analogWrite(firmataC, Int32(pin.number), Int32(width))
-  }
+    
+    public func configure(pin: Pin) {
+        firmata_pinMode(firmataC, Int32(pin.number), Int32(pin.mode.rawValue))
+    }
+    
+    public func digitalWrite(pin: Pin, level: DigitalLevel) {
+        firmata_digitalWrite(firmataC, Int32(pin.number), Int32(level.rawValue))
+    }
+    
+    public func analogWrite(pin: Pin, width: Int) {
+        firmata_analogWrite(firmataC, Int32(pin.number), Int32(width))
+    }
 }
 
 
