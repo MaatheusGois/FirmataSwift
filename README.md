d7701a0fb380c92166ab90cd8a8a935358ca146d
Gois <matheusgoislimasilva@gmail.com>
2019-11-06 18:04:58 -0300

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftFirmata
+
+A description of this package.
---
+++ Sources/SwiftFirmata/SwiftFirmata.swift
@@ -0,0 +1,52 @@
+import Firmata
+import Foundation
+
+public enum DigitalLevel: Int {
+  case Low, High
+}
+
+public enum PinMode: Int {
+  case Input, Output
+}
+
+public struct Pin {
+  let number: Int
+  let mode: PinMode
+}
+
+public enum SwiftFirmataError: Error {
+    case couldntConnect
+}
+
+public class SwiftFirmata {
+  private var firmataC: UnsafeMutablePointer<t_firmata>?
+
+  public init(connect: String, baud: Int) throws {
+    self.firmataC = firmata_new(strdup(connect))
+
+    if self.firmataC == nil {
+      throw SwiftFirmataError.couldntConnect
+    }
+  }
+
+  deinit {
+    if self.firmataC != nil {
+      free(self.firmataC)
+    }
+  }
+
+  public func configure(pin: Pin) {
+    firmata_pinMode(firmataC, Int32(pin.number), Int32(pin.mode.rawValue))
+  }
+
+  public func digitalWrite(pin: Pin, level: DigitalLevel) {
+    firmata_digitalWrite(firmataC, Int32(pin.number), Int32(level.rawValue))
+  }
+
+  public func analogWrite(pin: Pin, width: Int) {
+    firmata_analogWrite(firmataC, Int32(pin.number), Int32(width))
+  }
+}
+
+
+
